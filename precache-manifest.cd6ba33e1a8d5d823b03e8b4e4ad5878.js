c2c9c10d1448d2ccf1b44d9a426268259e27210b
DhirenParyani <sunny.paryani@gmail.com>
2020-09-08 16:45:57 -0700

    Updates

---
+++ precache-manifest.cd6ba33e1a8d5d823b03e8b4e4ad5878.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77ccafcc0b76043695afd17af3e44586",
+    "url": "/portfolio-app/index.html"
+  },
+  {
+    "revision": "bfacfd517c210e70650e",
+    "url": "/portfolio-app/static/css/main.e8c77071.chunk.css"
+  },
+  {
+    "revision": "b6ab53026d2fee289d41",
+    "url": "/portfolio-app/static/js/2.363fc51f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-app/static/js/2.363fc51f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfacfd517c210e70650e",
+    "url": "/portfolio-app/static/js/main.b8fa884f.chunk.js"
+  },
+  {
+    "revision": "9273fb07e8f782d2211c",
+    "url": "/portfolio-app/static/js/runtime-main.9d41a791.js"
+  },
+  {
+    "revision": "0cbf9de0b4dac7f1a806a9af99d77784",
+    "url": "/portfolio-app/static/media/Resume_400x400.0cbf9de0.png"
+  }
+]);
